e7b4efcca4dd62ed6ed187be6b3d533570157233
breno.lima <breno.lima@sefaz.pb.gov.br>
2022-11-21 15:43:13 -0300

    feat: Criação de novos components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,8 @@ import { DetailsComponent } from './pages/details/details.component';
 @NgModule({
   declarations: [
     AppComponent,
-    DetailsComponent
+    DetailsComponent,
+    
   ],
   imports: [
     BrowserModule,
