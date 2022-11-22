4a9a36a6d430d47bc1fe25b39e9fe7d17fa398ae
breno.lima <breno.lima@sefaz.pb.gov.br>
2022-11-22 13:49:48 -0300

    feat: Populando a lista de pokemons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
@@ -15,7 +15,8 @@ import { DetailsComponent } from './pages/details/details.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule
+    PagesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
