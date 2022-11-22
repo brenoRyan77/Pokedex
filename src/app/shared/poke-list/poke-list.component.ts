a36991afed561f088cbd04911626ef80580cd952
breno.lima <breno.lima@sefaz.pb.gov.br>
2022-11-22 14:29:39 -0300

    feat: Implementando filtragem na busca

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -9,15 +9,25 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
 export class PokeListComponent implements OnInit {
 
   public getAllPokemons: any;
+  private setAllPokemons: any;
 
   constructor(private pokeApiService: PokeApiService) { }
 
   ngOnInit(): void {
     this.pokeApiService.apiListAllPokemons.subscribe(
       res => {
-        this.getAllPokemons = res.results;
+        this.setAllPokemons = res.results;
+        this.getAllPokemons = this.setAllPokemons;
       }
     )
+  }
+
+  public getSearch(value: string){
+    const filter = this.setAllPokemons.filter((res: any) =>{
+      return !res.name.indexOf(value.toLowerCase())
+    })
+
+    this.getAllPokemons = filter;
   }
 
 }
